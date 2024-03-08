5faf51527ed1fa3df2d7af996db8f923e7ab2fb9
Nainsee Chauhan <nainsee02@gmail.com>
2024-03-08 12:57:39 +0530

    Update 2

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 import userModel from "../models/userModel.js";
-import { comparePassword, hashPassword } from "../utils/authhelper.js";
+import { comparePassword, hashPassword } from "../utils/authH elper.js";
 import JWT from "jsonwebtoken";
 
 export const registerController = async (req, res) => {
